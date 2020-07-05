424bc9fa61f0e7c48781f0c75ac0ad617497b415
schwamic <ms@tresmo.de>
2020-07-05 09:01:32 +0200

    docs: add comments to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 // cheatsheet: npx tailwind init tailwind-full.config.js --full
+// documentation: https://tailwindcss.com/docs/configuration/
 module.exports = {
   purge: ['./src/components/**/*.{js,jsx}', './src/pages/**/*.{js,jsx}'],
   theme: {
     // overwrite theme sections
     extend: {
-      // add variants to theme sections
+      // add styles to theme sections
     },
   },
   variants: {},
+  corePlugins: {},
   plugins: [],
 }
