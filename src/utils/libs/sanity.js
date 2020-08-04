ab15b9e08965198e4a1d647aa855c3d7f81b3466
schwamic <ms@tresmo.de>
2020-08-04 19:29:16 +0200

    chore: update deps

--- src/utils/libs/sanity.js
+++ src/utils/libs/sanity.js
@@ -3,6 +3,5 @@ import sanityClient from '@sanity/client'
 export default sanityClient({
   projectId: process.env.SANITY_PROJECT_ID,
   dataset: 'production',
-  // Use public mode because of caching ability for CDN
-  useCdn: true, // `false` if you want to ensure fresh data
+  useCdn: true, // Use public mode because of caching ability for CDN. `false` if you want to ensure fresh data
 })
