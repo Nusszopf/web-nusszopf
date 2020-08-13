a291efc919b7d5da3563367752a3a118ad5f224d
schwamic <ms@tresmo.de>
2020-08-13 19:19:08 +0200

    refactor: add input component

---
+++ src/components/atoms/Input.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
+
+export const INPUT_COLORS = {
+  yellow300blue400:
+    'text-yellow-300 placeholder-yellow-300 bg-blue-400 border-yellow-300 hover:shadow-outline:yellow-300 focus:placeholder-blue-400 focus:shadow-outline:yellow-300',
+}
+
+const Input = ({ disabled = false, color, className, ...props }) => (
+  <input
+    className={classnames(
+      'inline-block w-full px-5 py-4 text-lg transition-shadow duration-150 ease-in-out border-3 rounded-full shadow-xs appearance-none focus:outline-none ',
+      color,
+      className,
+      { 'opacity-50 cursor-not-allowed': disabled }
+    )}
+    disabled={disabled}
+    {...props}
+  />
+)
+
+Input.propTypes = {
+  color: PropTypes.oneOf(Object.values(INPUT_COLORS)).isRequired,
+  disabled: PropTypes.bool,
+  className: PropTypes.string,
+}
+
+export default Input
