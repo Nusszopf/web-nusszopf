4d8cdd25244f0aa593fdb426f8d2461597c05df1
schwamic <ms@tresmo.de>
2020-08-13 19:18:41 +0200

    refactor: add button component

---
+++ src/components/atoms/Button.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
+
+export const BTN_COLORS = {
+  whiteGray600: 'text-white bg-gray-600 hover:shadow-outline:gray-600 focus:shadow-outline:gray-600',
+  blue400Yellow300: 'text-blue-400 bg-yellow-300 hover:shadow-outline:yellow-300 focus:shadow-outline:yellow-300',
+}
+
+const Button = ({ className, color = BTN_COLORS.whiteGray600, label, ...props }) => (
+  <button
+    className={classnames(
+      'w-full py-4 text-lg font-bold transition-shadow duration-150 ease-in-out rounded-full outline-none sm:px-8 sm:w-auto focus:outline-none',
+      color,
+      className
+    )}
+    {...props}>
+    {label}
+  </button>
+)
+
+Button.propTypes = {
+  className: PropTypes.string,
+  color: PropTypes.string,
+  label: PropTypes.string.isRequired,
+}
+
+export default Button
