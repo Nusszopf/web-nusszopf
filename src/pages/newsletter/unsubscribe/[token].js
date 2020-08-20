c7a912779eb6b10a12f860903d36405c6c9c02c7
schwamic <ms@tresmo.de>
2020-08-20 08:25:48 +0200

    refactor: test

--- src/pages/newsletter/unsubscribe/[token].js
+++ src/pages/newsletter/unsubscribe/[token].js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { Page } from '../../../containers'
+import { confirmNewsletterUnsubscription } from '../../../utils/services/newsletter.service'
 
 const UnsubscribeConfirm = () => {
   const router = useRouter()
@@ -58,3 +59,22 @@ const UnsubscribeConfirm = () => {
 // TODO: server fetch...
 
 export default UnsubscribeConfirm
+
+export const getServerSideProps = async ctx => {
+  const { token } = ctx.query
+  const response = await confirmNewsletterUnsubscription(token)
+  console.log(response)
+  if (response.ok) {
+    const lead = await response.json()
+    return { props: { lead } }
+  } else {
+    // return { props: { lead: { email: 'finn@nuss.de', name: 'Finn' } } }
+    // ctx.res.writeHead(307, { Location: '/404?asdf' })
+    // ctx.res.end()
+    // return { props: { statusCode: 404 } }
+    console.log('throw error')
+    const err = new Error()
+    err.code = 'ENOENT'
+    throw err
+  }
+}
