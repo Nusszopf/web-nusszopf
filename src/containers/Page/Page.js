ee099d217d46a86e2137fb47760393cddd3150cd
schwamic <ms@tresmo.de>
2020-08-27 22:27:55 +0200

    fix: seo

--- src/containers/Page/Page.js
+++ src/containers/Page/Page.js
@@ -38,7 +38,7 @@ const Page = ({
         title={truncate(title, { length: 60 })}
         description={truncate(description, { length: 150 })}
         canonical={canonical}
-        noindex={process.env.NODE_ENV !== 'production' ? true : noindex}
+        noindex={process.env.ENV !== 'production' ? true : noindex}
         openGraph={{
           description,
           images: [
