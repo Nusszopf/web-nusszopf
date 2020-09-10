40b5c0869f75096cfbcb24a7af573abf0e51b647
schwamic <ms@tresmo.de>
2020-09-10 10:35:32 +0200

    fix: testing

--- src/pages/api/newsletter/unsubscribe-confirm.js
+++ src/pages/api/newsletter/unsubscribe-confirm.js
@@ -8,16 +8,27 @@ import { DELETE_LEAD } from '../../../utils/hasura/mutations/newsletter.mutation
 const ERROR_CONSTRAINT = 'constraint-violation'
 
 export default async function unsubscribeConfirm(req, res) {
-  await runMiddleware(req, res, rateLimiter)
+  // await runMiddleware(req, res, rateLimiter)
   const { token } = req.body
   sgClient.setApiKey(process.env.SENDGRID_API_KEY)
+  const reqContact = {
+    method: 'GET',
+    url: 'https://api.sendgrid.com/v3/marketing/lists',
+    qs: { page_size: '100' },
+    json: true,
+  }
+  const [response, body] = await sgClient.request(reqContact)
+  console.log(body)
   try {
     const { leadId } = jwt.verify(token, process.env.EMAIL_SECRET)
     const lead = await deleteLead(leadId)
+    console.log(lead)
     if (lead) {
       const contactId = await getContactId(sgClient, lead.email, process.env.SENDGRID_LIST_ID)
+      console.log(contactId)
       if (contactId) {
-        await deleteContact(sgClient, contactId)
+        const request = await deleteContact(sgClient, contactId)
+        console.log(request)
         res.status(200).json({ email: lead.email, name: lead.name })
       } else {
         res.status(404).end(`lead with id ${leadId} was not found in sendgrid`)
@@ -40,6 +51,8 @@ const deleteLead = async id => {
   return res?.delete_leads_by_pk
 }
 
+// TODO
+// SendGrid returns []... list_id query nochmal überprüfen...
 const getContactId = async (sgClient, email, listId) => {
   const reqContact = {
     method: 'POST',
@@ -50,6 +63,8 @@ const getContactId = async (sgClient, email, listId) => {
     json: true,
   }
   const [response, body] = await sgClient.request(reqContact)
+  console.log(response)
+  console.log(body)
   return body.result[0]?.id
 }
 
