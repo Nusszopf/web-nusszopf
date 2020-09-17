5550aef79cf812a2276a671ae1bb8d1dd8f1f3fe
schwamic <ms@tresmo.de>
2020-09-18 01:06:12 +0200

    refactor: analytics test

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -26,7 +26,7 @@ class CustomDocument extends Document {
           <link href="/favicons/favicon.ico" rel="shortcut icon" />
           <script
             dangerouslySetInnerHTML={{
-              __html: `(function(v,i,s,a){if(!v._visaSettings){v._visaSettings={};}v._visaSettings["4786e4f4-f91c-11ea-b589-901b0edac50a"]={v:"0.3",s:"4786e4f4-f91c-11ea-b589-901b0edac50a",a:"1"};_v=i.getElementsByTagName("head")[0];_a=_v;_i=i.createElement("script");_s=_i;_s.defer="defer";_s.src=s+a+v._visaSettings["4786e4f4-f91c-11ea-b589-901b0edac50a"].v;_a.appendChild(_s);})(window,document,"//app-worker.visitor-analytics.io/main",".js?s=4786e4f4-f91c-11ea-b589-901b0edac50a&v=")`,
+              __html: `(function(v,i,s,a){if(!v._visaSettings){v._visaSettings={};}v._visaSettings["3e1ed546-f93a-11ea-b589-901b0edac50a"]={v:"0.3",s:"3e1ed546-f93a-11ea-b589-901b0edac50a",a:"1"};_v=i.getElementsByTagName("head")[0];_a=_v;_i=i.createElement("script");_s=_i;_s.defer="defer";_s.src=s+a+v._visaSettings["3e1ed546-f93a-11ea-b589-901b0edac50a"].v;_a.appendChild(_s);})(window,document,"//app-worker.visitor-analytics.io/main",".js?s=3e1ed546-f93a-11ea-b589-901b0edac50a&v=")`,
             }}
           />
         </Head>
