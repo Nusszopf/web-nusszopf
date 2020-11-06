37b06793f0f9375de22c6f045d9537d3ff744182
schwamic <ms@tresmo.de>
2020-11-06 18:14:53 +0100

    refactor: menu

--- projects/ui-library/stories/molecules/Menu/Menu.theme.js
+++ projects/ui-library/stories/molecules/Menu/Menu.theme.js
@@ -5,11 +5,11 @@ export const MenuVariant = {
 
 export const MenuColor = {
   gray200: {
-    menu: 'text-gray-600 bg-gray-200 border-2 border-gray-200',
+    menu: 'text-gray-600 bg-gray-200',
     item: 'hover:bg-gray-300 focus:bg-gray-300',
   },
   lilac700: {
-    menu: 'text-white bg-lilac-700 border-2 border-lilac-700',
+    menu: 'text-white bg-lilac-700',
     item: 'hover:bg-lilac-600 focus:lilac-600',
   },
 }
