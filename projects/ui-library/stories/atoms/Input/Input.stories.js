1b0de31630b1ca8e42ea689bfdde079d19321bc3
schwamic <ms@tresmo.de>
2020-11-06 18:31:25 +0100

    refactor: input textarea

--- projects/ui-library/stories/atoms/Input/Input.stories.js
+++ projects/ui-library/stories/atoms/Input/Input.stories.js
@@ -7,12 +7,14 @@ export default {
   parameters: {
     docs: {
       description: {
-        component: '**UI atom** based on [Reakit Input](https://reakit.io/docs/input/).',
+        component:
+          '**Input Atom** based on [Reakit Input](https://reakit.io/docs/input/). Renders as TextArea if `as="textarea"` will be passed.',
       },
     },
   },
 }
 
+export const Area = () => <Input as="textarea" placeholder="text area" />
 export const Base = () => <Input placeholder="base size" />
 export const Large = () => <Input size="large" placeholder="large size" />
 export const Disabled = () => <Input disabled color="yellow300Blue400" placeholder="is disabled" />
