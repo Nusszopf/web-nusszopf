7c6b7ed9aeed7ff09d8ce16f4366ed25fa8a3eea
schwamic <ms@tresmo.de>
2020-11-15 14:29:57 +0100

    refactor: avatar truncate

--- projects/webapp/src/containers/Avatar/Avatar.js
+++ projects/webapp/src/containers/Avatar/Avatar.js
@@ -1,13 +1,14 @@
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
+import { truncate } from 'lodash'
 import { Text } from 'ui-library/stories/atoms'
 
 const Avatar = ({ user, className, ...props }) => (
   <div className={classnames('flex items-center break-all', className)} {...props}>
     <img className="w-16 h-16 bg-gray-600 rounded-full" src={user?.auth?.picture} alt="avatar" />
     <div className="ml-4">
-      <Text variant="textSmMedium">{user?.auth?.name ?? '-'}</Text>
-      {user?.data?.email && <Text variant="textSm">{user?.data?.email}</Text>}
+      <Text variant="textSmMedium">{truncate(user?.auth?.name ?? '-', { length: 33 })}</Text>
+      {user?.data?.email && <Text variant="textSm">{truncate(user?.data?.email ?? '-', { length: 33 })}</Text>}
     </div>
   </div>
 )
