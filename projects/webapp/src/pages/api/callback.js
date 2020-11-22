5343b571472afc20757d94faf886533736a214e0
schwamic <ms@tresmo.de>
2020-11-22 09:46:38 +0100

    refactor: redirect after login

--- projects/webapp/src/pages/api/callback.js
+++ projects/webapp/src/pages/api/callback.js
@@ -2,7 +2,7 @@ import auth0 from '../../utils/libs/auth0'
 
 export default async function callback(req, res) {
   try {
-    await auth0.handleCallback(req, res, { redirectTo: '/' })
+    await auth0.handleCallback(req, res, { redirectTo: '/user/profile' })
   } catch (error) {
     console.error(error)
     res.status(error.status || 500).end(error.message)
