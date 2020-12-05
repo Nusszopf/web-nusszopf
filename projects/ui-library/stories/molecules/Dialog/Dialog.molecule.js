70ca1d2519b5cfc93b987676865dfd4e63caab78
schwamic <ms@tresmo.de>
2020-12-05 14:39:37 +0100

    fix: dialog height

--- projects/ui-library/stories/molecules/Dialog/Dialog.molecule.js
+++ projects/ui-library/stories/molecules/Dialog/Dialog.molecule.js
@@ -5,7 +5,9 @@ import { Dialog as RDialog } from '@reach/dialog'
 
 const Dialog = ({ children, isOpen, close, className, ...props }) => (
   <RDialog isOpen={isOpen} onDismiss={close} {...props}>
-    <div className={classnames('w-screen h-screen sm:h-auto p-4 sm:rounded-lg sm:max-w-xl', className)}>{children}</div>
+    <div className={classnames('w-screen h-auto min-h-screen sm:min-h-0 p-4 sm:rounded-lg sm:max-w-xl', className)}>
+      {children}
+    </div>
   </RDialog>
 )
 
