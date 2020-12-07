7b9edc5bb0d72e1724097ed4b625cd3943d2b27c
schwamic <ms@tresmo.de>
2020-12-07 21:33:21 +0100

    fix(framefullcenter): story

--- projects/ui-library/stories/templates/FrameFullCenter/FrameFullCenter.stories.js
+++ projects/ui-library/stories/templates/FrameFullCenter/FrameFullCenter.stories.js
@@ -7,32 +7,16 @@ export default {
   parameters: {
     docs: {
       description: {
-        component: 'A wrapper for pages: full-height, full-screen and flex-positions.',
+        component: 'A container inside pages. Helpful for fullscreen pages with centered content.',
       },
     },
   },
 }
 
-export const Primary = () => (
+export const Main = () => (
   <div className="h-screen">
     <FrameFullCenter className="bg-gray-100">
-      <div className="p-4 text-white bg-gray-300">centered content</div>
-    </FrameFullCenter>
-  </div>
-)
-
-export const With_Flex = () => (
-  <div className="h-screen">
-    <FrameFullCenter className="bg-gray-100" flex="items-center justify-end">
-      <div className="p-4 text-white bg-gray-300">centered content</div>
-    </FrameFullCenter>
-  </div>
-)
-
-export const With_Footer = () => (
-  <div className="h-screen">
-    <FrameFullCenter className="bg-gray-100" brand={<div className="text-center text-white bg-gray-500">footer</div>}>
-      <div className="p-4 text-white bg-gray-300">centered content</div>
+      <div className="text-white bg-gray-300">centered content</div>
     </FrameFullCenter>
   </div>
 )
