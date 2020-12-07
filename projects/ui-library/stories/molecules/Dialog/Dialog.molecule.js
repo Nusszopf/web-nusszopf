ff69b874525955e94d7e743fbb2e84dcfa37028a
schwamic <ms@tresmo.de>
2020-12-07 19:01:45 +0100

    fix: dialog molecule

--- projects/ui-library/stories/molecules/Dialog/Dialog.molecule.js
+++ projects/ui-library/stories/molecules/Dialog/Dialog.molecule.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types'
 import classnames from 'classnames'
 import { Dialog as RDialog } from '@reach/dialog'
 
-const Dialog = ({ children, isOpen, close, className, ...props }) => (
-  <RDialog isOpen={isOpen} onDismiss={close} {...props}>
+const Dialog = ({ children, isOpen, onDismiss, className, ...props }) => (
+  <RDialog isOpen={isOpen} onDismiss={onDismiss} {...props}>
     <div className={classnames('w-screen h-auto min-h-screen sm:min-h-0 p-4 sm:rounded-lg sm:max-w-xl', className)}>
       {children}
     </div>
@@ -15,7 +15,7 @@ Dialog.propTypes = {
   className: PropTypes.string,
   children: PropTypes.node,
   isOpen: PropTypes.bool,
-  close: PropTypes.func,
+  onDismiss: PropTypes.func,
 }
 
 export default Dialog
