ad9984f8edbca822280a23f2573d5edadabdbe62
schwamic <ms@tresmo.de>
2021-01-10 13:21:55 +0100

    chore: update deps and version

---
+++ projects/ui-library/index.js
@@ -0,0 +1,4 @@
+// Next-Transpile-Modules (Version 5+)
+// The src in the main field specifies the root node for next-transpile-modules.
+
+// More infos: https://github.com/martpie/next-transpile-modules/releases/tag/5.0.0
