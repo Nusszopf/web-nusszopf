c9bd9e0d12a0f31f3a8ced8bf08532a3f3ebf9fe
schwamic <ms@tresmo.de>
2021-01-15 00:50:27 +0100

    fix: show how to section

--- projects/webapp/src/pages/index.js
+++ projects/webapp/src/pages/index.js
@@ -30,7 +30,7 @@ const Index = () => (
         </div>
       </div>
     </Frame>
-    {process.env.ENV !== 'production' && <HowToSection />}
+    <HowToSection />
     {process.env.ENV !== 'production' && <CarouselSection />}
     <Frame className="pt-12 pb-16 bg-turquoise-300 sm:pt-16 sm:pb-18 xl:pt-18 xl:pb-20">
       <Text as="h3" variant="titleMd" className="mb-8 sm:max-w-sm xl:max-w-full xl:mb-10">
