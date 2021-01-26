6f529349a75e71d72e89fe05374b0841d708bfd7
schwamic <ms@tresmo.de>
2021-01-26 21:00:27 +0100

    fix(upload): vercel test

--- projects/webapp/src/utils/libs/s3.js
+++ projects/webapp/src/utils/libs/s3.js
@@ -1,10 +1,21 @@
-import aws from 'aws-sdk'
+const aws = require('aws-sdk')
 
-const s3 = new aws.S3({
-  endpoint: process.env.SPACES_ENDPOINT,
+aws.config.update({
   accessKeyId: process.env.SPACES_ACCESS_KEY,
   secretAccessKey: process.env.SPACES_SECRET_KEY,
+  // region: process.env.AWS_REGION_MYAPP,
+  bucketname: process.env.BUCKET_NAME,
+  endpoint: process.env.SPACES_ENDPOINT,
   signatureVersion: 'v4',
 })
+
+// const s3 = new aws.S3({
+//   endpoint: process.env.SPACES_ENDPOINT,
+//   accessKeyId: process.env.SPACES_ACCESS_KEY,
+//   secretAccessKey: process.env.SPACES_SECRET_KEY,
+//   signatureVersion: 'v4',
+// })
+
+const s3 = new aws.S3()
 
 export default s3
