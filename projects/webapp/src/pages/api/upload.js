6127db9608dcaf74f593f4c7da39e7e60e2584e9
schwamic <ms@tresmo.de>
2021-01-26 21:48:49 +0100

    fix: undo vercel changes

--- projects/webapp/src/pages/api/upload.js
+++ projects/webapp/src/pages/api/upload.js
@@ -1,36 +1,13 @@
-// import s3 from '../../utils/libs/s3'
-const aws = require('aws-sdk')
-
+import s3 from '../../utils/libs/s3'
 import auth0 from '../../utils/libs/auth0'
 import runMiddleware, { rateLimiter } from '../../utils/functions/runMiddleware.function'
 import { ERROR_CONSTRAINT } from '../../utils/enums'
 
-// aws.config.update({
-//   accessKeyId: process.env.SPACES_ACCESS_KEY,
-//   secretAccessKey: process.env.SPACES_SECRET_KEY,
-//   region: 'fra1',
-//   bucketname: process.env.BUCKET_NAME,
-//   endpoint: process.env.SPACES_ENDPOINT,
-//   signatureVersion: 'v4',
-// })
-
-const spacesEndpoint = new aws.Endpoint(process.env.SPACES_ENDPOINT)
-const s3 = new aws.S3({
-  endpoint: spacesEndpoint,
-  accessKeyId: process.env.SPACES_ACCESS_KEY,
-  secretAccessKey: process.env.SPACES_SECRET_KEY,
-})
-
 export default auth0.requireAuthentication(async function upload(req, res) {
-  console.log(process.env.SPACES_ENDPOINT)
-  console.log(process.env.SPACES_ACCESS_KEY)
-  console.log(process.env.SPACES_SECRET_KEY)
   try {
     await runMiddleware(req, res, rateLimiter)
     const { id, picture } = req.body
-    console.log(id, picture)
-    console.log('log2', picture?.includes('nz_v'))
-    const isFirstUpload = !picture?.includes('nz_v') // todo führt zu false???
+    const isFirstUpload = !picture?.includes('nz_v')
     const filename = createFilename(picture, id, isFirstUpload)
     const post = await s3.createPresignedPost({
       Bucket: process.env.BUCKET_NAME,
@@ -44,17 +21,17 @@ export default auth0.requireAuthentication(async function upload(req, res) {
       ],
     })
 
-    console.log('log3', decodeURIComponent(picture.split('com/')[1]))
     // todo start -> outsoure as event: 'cleanup-pictures-digitalocean'
     if (!isFirstUpload) {
-      const key = decodeURIComponent(picture.split('com/')[1]) // todo check if key is okay...
-      console.log('log4', key)
+      const key = decodeURIComponent(picture.split('com/')[1])
       await s3.deleteObject(
         {
           Bucket: process.env.BUCKET_NAME,
           Key: key,
         },
-        () => {}
+        () => {
+          return
+        }
       )
     }
     // todo end
--- projects/webapp/src/utils/libs/s3.js
+++ projects/webapp/src/utils/libs/s3.js
@@ -1,21 +1,10 @@
-const aws = require('aws-sdk')
+import aws from 'aws-sdk'
 
-aws.config.update({
+const s3 = new aws.S3({
+  endpoint: process.env.SPACES_ENDPOINT,
   accessKeyId: process.env.SPACES_ACCESS_KEY,
   secretAccessKey: process.env.SPACES_SECRET_KEY,
-  // region: process.env.AWS_REGION_MYAPP,
-  bucketname: process.env.BUCKET_NAME,
-  endpoint: process.env.SPACES_ENDPOINT,
   signatureVersion: 'v4',
 })
-
-// const s3 = new aws.S3({
-//   endpoint: process.env.SPACES_ENDPOINT,
-//   accessKeyId: process.env.SPACES_ACCESS_KEY,
-//   secretAccessKey: process.env.SPACES_SECRET_KEY,
-//   signatureVersion: 'v4',
-// })
-
-const s3 = new aws.S3()
 
 export default s3
