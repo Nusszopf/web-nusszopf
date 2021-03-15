82dfe04fc5aad3cba83837444d785a35eeea4c78
Michael Schwarz <schwarz.michael@posteo.net>
2021-03-15 21:34:10 +0100

    fix: responsive links and routes

--- projects/ui-library/stories/atoms/Link/Link.atom.js
+++ projects/ui-library/stories/atoms/Link/Link.atom.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
 import { Button, Text } from '../../atoms'
@@ -6,83 +6,90 @@ import { ButtonColor, ButtonVariant } from '../Button/Button.theme'
 import { TextVariant } from '../Text/Text.theme'
 import { LinkColor, LinkBorder, LinkVariant, LinkType } from './Link.theme'
 
-const Link = ({
-  children,
-  href,
-  ariaLabel,
-  className,
-  title,
-  variant = 'text',
-  buttonVariant = 'filled',
-  textVariant = 'textMd',
-  color = 'steel',
-  border = 'medium',
-  type = 'url',
-  ...props
-}) => {
-  switch (variant) {
-    case LinkVariant.text: {
-      return (
-        // eslint-disable-next-line react/jsx-no-target-blank
-        <a
-          className={classnames('cursor-pointer group', className)}
-          href={encodeURI(href)}
-          rel={type === LinkType.url ? 'noopener noreferrer' : null}
-          target={type === LinkType.url ? '_blank' : null}
-          title={title}
-          aria-label={ariaLabel}
-          {...props}>
+const Link = forwardRef(
+  (
+    {
+      children,
+      href,
+      ariaLabel,
+      className,
+      title,
+      variant = 'text',
+      buttonVariant = 'filled',
+      textVariant = 'textMd',
+      color = 'steel',
+      border = 'medium',
+      type = 'url',
+      ...props
+    },
+    ref
+  ) => {
+    switch (variant) {
+      case LinkVariant.text: {
+        return (
+          // eslint-disable-next-line react/jsx-no-target-blank
           <Text
-            as="span"
+            as="a"
+            ref={ref}
             variant={textVariant}
             className={classnames(
-              'inline-block',
+              'cursor-pointer',
               {
                 'border-b': border === LinkBorder.small,
                 'border-b-2': border === LinkBorder.medium,
                 'border-b-3': border === LinkBorder.large,
               },
-              LinkColor[color]
-            )}>
+              LinkColor[color],
+              className
+            )}
+            href={encodeURI(href)}
+            rel={type === LinkType.url ? 'noopener noreferrer' : null}
+            target={type === LinkType.url ? '_blank' : null}
+            title={title}
+            aria-label={ariaLabel}
+            {...props}>
             {children}
           </Text>
-        </a>
-      )
-    }
-    case LinkVariant.svg: {
-      return (
-        <a
-          className={classnames('cursor-pointer', className)}
-          href={encodeURI(href)}
-          rel="noopener noreferrer"
-          target="_blank"
-          title={title}
-          aria-label={ariaLabel}
-          {...props}>
-          {children}
-        </a>
-      )
-    }
-    case LinkVariant.button: {
-      return (
-        <Button
-          as="a"
-          color={color}
-          className={classnames('inline-block cursor-pointer', className)}
-          href={encodeURI(href)}
-          rel={type === LinkType.url ? 'noopener noreferrer' : null}
-          target={type === LinkType.url ? '_blank' : null}
-          title={title}
-          aria-label={ariaLabel}
-          variant={buttonVariant}
-          {...props}>
-          {children}
-        </Button>
-      )
+        )
+      }
+      case LinkVariant.svg: {
+        return (
+          <a
+            ref={ref}
+            className={classnames('cursor-pointer', className)}
+            href={encodeURI(href)}
+            rel="noopener noreferrer"
+            target="_blank"
+            title={title}
+            aria-label={ariaLabel}
+            {...props}>
+            {children}
+          </a>
+        )
+      }
+      case LinkVariant.button: {
+        return (
+          <Button
+            as="a"
+            ref={ref}
+            color={color}
+            className={classnames('inline-block cursor-pointer', className)}
+            href={encodeURI(href)}
+            rel={type === LinkType.url ? 'noopener noreferrer' : null}
+            target={type === LinkType.url ? '_blank' : null}
+            title={title}
+            aria-label={ariaLabel}
+            variant={buttonVariant}
+            {...props}>
+            {children}
+          </Button>
+        )
+      }
     }
   }
-}
+)
 
+Link.displayName = 'Link'
 Link.propTypes = {
   ariaLabel: PropTypes.string.isRequired,
   border: PropTypes.oneOf(Object.keys(LinkBorder)),
