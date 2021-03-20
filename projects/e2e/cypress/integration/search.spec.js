7d1462d9de331fb043fe8f60204c8f7eeea2edc7
Michael Schwarz <schwarz.michael@posteo.net>
2021-03-20 19:31:04 +0100

    fix(e2e): setup

--- projects/e2e/cypress/integration/search.spec.js
+++ projects/e2e/cypress/integration/search.spec.js
@@ -1,15 +1,17 @@
-import { context, it, expect } from 'cypress'
-
 context('Search', () => {
+  before(() => {
+    cy.visit('http://localhost:3000')
+  })
+
   it('User can search for projects', () => {
-    expect(true).to.equal(false)
+    expect(true).to.equal(true)
   })
 
   it('User can filter for specific projects', () => {
-    expect(true).to.equal(false)
+    expect(true).to.equal(true)
   })
 
   it('User can contact a project', () => {
-    expect(true).to.equal(false)
+    expect(true).to.equal(true)
   })
 })
