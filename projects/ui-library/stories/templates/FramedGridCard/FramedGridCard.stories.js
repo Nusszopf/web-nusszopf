b07064736a747feee3dd60f82e1bbc277cdb6870
Michael Schwarz <schwarz.michael@posteo.net>
2021-04-30 16:41:10 +0200

    chore: update deps

--- projects/ui-library/stories/templates/FramedGridCard/FramedGridCard.stories.js
+++ projects/ui-library/stories/templates/FramedGridCard/FramedGridCard.stories.js
@@ -15,31 +15,33 @@ export default {
 
 export const Default = () => (
   <div className="pb-12 bg-stone-300">
-    <div className="pb-12 bg-stone-300">
-      <FramedGridCard
-        className="lg:pt-12"
-        bodyColor="bg-white lg:bg-stone-300"
-        headerColor="bg-stone-300 lg:bg-stone-300">
-        <FramedGridCard.Header className="bg-stone-600">header</FramedGridCard.Header>
-        <FramedGridCard.Body className="bg-white">
-          <FramedGridCard.Body.Col className="bg-red-300 ">col</FramedGridCard.Body.Col>
-          <FramedGridCard.Body.Col className="bg-yellow-300 ">col</FramedGridCard.Body.Col>
-        </FramedGridCard.Body>
-      </FramedGridCard>
-    </div>
+    <FramedGridCard
+      className="lg:py-12"
+      bodyColor="bg-white lg:bg-stone-300"
+      headerColor="bg-stone-300 lg:bg-stone-300">
+      <FramedGridCard.Header className="bg-stone-600">header</FramedGridCard.Header>
+      <FramedGridCard.Body className="bg-white">
+        <FramedGridCard.Body.Col className="bg-red-300 ">col</FramedGridCard.Body.Col>
+        <FramedGridCard.Body.Col className="bg-yellow-300 ">col</FramedGridCard.Body.Col>
+      </FramedGridCard.Body>
+    </FramedGridCard>
   </div>
 )
 
 export const Two_Cols = () => (
   <div className="pb-12 bg-stone-300">
     <FramedGridCard
-      className="lg:pt-12"
+      className="lg:py-12"
       bodyColor="bg-white lg:bg-stone-300"
       headerColor="bg-stone-300 lg:bg-stone-300">
       <FramedGridCard.Header className="bg-stone-600">header</FramedGridCard.Header>
-      <FramedGridCard.Body variant="twoCols" className="bg-white">
-        <FramedGridCard.Body.Col className="bg-red-300 ">col</FramedGridCard.Body.Col>
-        <FramedGridCard.Body.Col className="bg-yellow-300 ">col</FramedGridCard.Body.Col>
+      <FramedGridCard.Body className="bg-white">
+        <FramedGridCard.Body.Col variant="twoCols" className="bg-red-300 lg:col-start-2">
+          col
+        </FramedGridCard.Body.Col>
+        <FramedGridCard.Body.Col variant="twoCols" className="bg-yellow-300">
+          col
+        </FramedGridCard.Body.Col>
       </FramedGridCard.Body>
     </FramedGridCard>
   </div>
